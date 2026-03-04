c8f8b2c8e574417eb7bd34b54238f571b553ad3b
Christian NGUYEN <christian.nguyen.31@gmail.com>
2026-03-04 07:33:49 +0100

    Step_9: TEXTSPLIT - Refactor - Remove unnecessary row delimiters temp variable

--- src/text.js
+++ src/text.js
@@ -652,7 +652,6 @@ export function TEXTSPLIT(text, col_delimiter, row_delimiter) {
   }
 
   const delimiters = []
-  const row_delimiters = []
   const col_delimiters = []
 
   if (utils.isDefined(col_delimiter)) {
@@ -666,9 +665,6 @@ export function TEXTSPLIT(text, col_delimiter, row_delimiter) {
 
   if (utils.isDefined(row_delimiter)) {
     delimiters.push(
-      ...(!Array.isArray(row_delimiter) ? [ row_delimiter ] : row_delimiter)
-    )
-    row_delimiters.push(
       ...(!Array.isArray(row_delimiter) ? [ row_delimiter ] : row_delimiter)
     )
   }
@@ -704,7 +700,7 @@ export function TEXTSPLIT(text, col_delimiter, row_delimiter) {
         const matchedPattern = currentMatch[0]
         const textBeforeMatch = text.slice(lastIndex, regex.lastIndex - matchedPattern.length)
         currentRow.push(textBeforeMatch)
-        if (row_delimiters.includes(matchedPattern) && !col_delimiters.includes(matchedPattern)) {
+        if (!col_delimiters.includes(matchedPattern)) {
           result.push(currentRow)
           colMax = Math.max(colMax, currentRow.length)
           currentRow = []
